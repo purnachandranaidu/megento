7858e3b92e0604c98580307703b89fe97340c23a
purnapcn@gmail.com <purnapcn@gmail.com>
2019-06-16 21:40:54 +0530

    New Commit

--- src/main/java/com/Wonderwe/PageObjects/Animals.java
+++ src/main/java/com/Wonderwe/PageObjects/Animals.java
@@ -1,5 +1,6 @@
 package com.Wonderwe.PageObjects;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -8,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import com.Wonderwe.TestCases.BaseClass;
 
@@ -23,11 +25,36 @@ public class Animals extends BaseClass {
 	
 
 	@FindBy(xpath="(//div[@class='homePage-title'])[1]") WebElement Aanimals;
+	@FindBy(xpath="//A[@href='/start/?category=1'][text()='Start an animal and nature fundraiser']") WebElement AanimalnatureFund;
+	@FindBy(xpath="//SELECT[@id='COUNTRY']") WebElement Country;
 	
 	public void Animal_page() throws Exception
 	{
 		Thread.sleep(1000);
 		Aanimals.click();
 		
+	}
+	
+	public void Animal_fundriser() throws Exception
+	{
+		Thread.sleep(1000);
+		AanimalnatureFund.click();
+		
+	}
+	
+	public void Country() throws InterruptedException
+	{
+		Thread.sleep(1000);
+		Select country = new Select(driver.findElement(By.id("COUNTRY")));
+		Thread.sleep(1000);
+		List<WebElement> list=driver.findElements(By.tagName("option"));
+		list.size();
+		System.out.println(list.size());
+		for (WebElement option:list) {
+			System.out.println(option.getText());
+			list.get(2);
+			break;
+			
+		}
 	}
 }
--- src/test/java/com/Wonderwe/TestCases/TC_002_AnimalsPage.java
+++ src/test/java/com/Wonderwe/TestCases/TC_002_AnimalsPage.java
@@ -1,18 +1,21 @@
 package com.Wonderwe.TestCases;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import com.Wonderwe.PageObjects.Animals;
 
 public class TC_002_AnimalsPage extends BaseClass {
 	
 	@Test(priority=2)
-	public void Animalsss() throws Exception
+	public  void Animalsss() throws Exception
 	{
 		try {
 			
@@ -21,7 +24,7 @@ public class TC_002_AnimalsPage extends BaseClass {
 			animals.Animal_page();
 			
 			String Act=driver.findElement(By.xpath("//div[@class='bigtext']/h1")).getText();
-			if(Act.equals("Animalsss and Nature"))
+			if(Act.equals("Animals and Nature"))
 			{
 				Assert.assertTrue(true);
 				System.out.println("Page Navigated Successfully");
@@ -41,6 +44,46 @@ public class TC_002_AnimalsPage extends BaseClass {
 			Assert.fail(e.getMessage());
 			
 		}
+	}
+	
+	@Test(priority=3)
+	public void AnimalNatureFundriser() throws IOException
+	{
+		try {
+			Animals animals=new Animals();
+			animals.Animal_fundriser();
+			WebElement  Act=driver.findElement(By.name("subscribe"));
+			
+			
+			if(Act.equals(Act))
+			{
+				Assert.assertTrue(true);
+			}
+			else
+			{
+				throw new Exception("Element Not Found");
+			}
+			
+			
+			
+		} catch (Exception e) {
+			
+			errorMsg=e.getMessage();
+			timestamp=new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+			capturescreen(driver,timestamp);
+			System.out.println(e.getMessage());
+			//Assert.assertTrue(false);
+			Assert.fail(e.getMessage());
+			
+		}
+		
+	}
+	
+	@Test
+	public void CountrySelect() throws InterruptedException
+	{
+		Animals animals=new Animals();
+		animals.Country();
 	}
 
 }
